e800a909171741b30a110965b2cd632b3e6360cd
Inhame <gabrielianhez@gmail.com>
2019-05-02 14:07:31 -0300

    'Add'

---
+++ Script/main.js
@@ -0,0 +1,28 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'Images/Foto1.PNG') {
+      myImage.setAttribute ('src','Images/Foto2.PNG');
+    } else {
+      myImage.setAttribute ('src','Images/Foto1.PNG');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Qual seu nome?');
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Bem-Vindoa, ' + myName;
+  }
+  if(!localStorage.getItem('name') || localStorage.getItem('name')==='null') {
+    setUserName();
+  } else {
+    var storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Bem-Vindo, ' + storedName;
+  }
+  myButton.onclick = function() {
+    setUserName();
+  }
